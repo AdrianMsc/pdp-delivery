7e726abb7aaeef7cd61dc00eefb2c2687703964f
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-02-26 12:15:00 -0600

    separated wyas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./col-1.html",
+    "./col-2.html",
+    "./col-3.html",
+    "./src/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
