9a4335fe22ae7c12526f32bce40807bba9cf6bb3
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-03-03 10:28:08 -0600

    tw-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./col-1.html",
-    "./col-2.html",
-    "./col-3.html",
-    "./src/**/*.{html,js}",
+    './index.html',
+    './documents.html',
+    './product-details.html',
+    './src/**/*.{html,js}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
